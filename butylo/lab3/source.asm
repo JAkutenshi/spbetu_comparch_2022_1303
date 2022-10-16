bb254632a60428bf1c7025a66c8c85e650ad6fa2
feyhoa <eabutylo@stud.eltech.ru>
2022-10-16 18:20:35 +0300

    done

--- butylo/lab3/source.asm
+++ butylo/lab3/source.asm
@@ -22,7 +22,6 @@ CODE SEGMENT
 Main      PROC  FAR
 	mov   AX,DATA
 	mov   DS,AX
-
 ;Вычисление f1 и f2
 	mov ax,a	;заносим значение а в ах
 	mov cx,i	;заносим i в cx
@@ -32,14 +31,12 @@ Main      PROC  FAR
 				;если a<=b
 	sal cx,1	;умножение i на 2 cx = i*2
 	add cx,i	;cx = 2*i + i = 3*i	
-	mov ax,4	;ax = 4
-	add cx,ax	;cx = 3*i + 4
+	add cx,4	;cx = 3*i + 4
 	mov i1,cx	;сохранение результата в f1
 	
-	add cx,ax	;cx = 3*i + 4 + 2 = 3(i + 2)
+	add cx,2	;cx = 3*i + 4 + 2 = 3(i + 2)
 	mov i2,cx	;сохраняем рез-т в f2
 	jmp A2		;Пропускаем следующие шаги
-
 A1:				;если a>b
 	mov cx,i	;восстановление значения i в cx	
 	sal cx,1	;cx = i*2
@@ -53,11 +50,9 @@ A1:				;если a>b
 	mov ax,4	;ax = 4
 	sub ax,cx	;ax = ax - cx = 4 - 6*i 
 	mov i2,ax	;сохраняем результат в f2
-	
 ;Вычисление f3
 A2:
 	mov ax,k
-	
 	cmp ax,0	;сравниваем k и 0
 	JNe B1		;если k не равно 0 то перйти на B1
 	
@@ -70,24 +65,19 @@ A2:
 	neg dx		;если i1 + i2 < 0 то меняем знак на противоположный
 	mov res,dx	;res = dx
 	jmp B2
-	
 C1:
 	mov res,dx
 	jmp B2
-	
 B1:
-				;если k не равно 0
-	mov ax,i1
+	mov ax,i1	;если k не равно 0
 	mov bx,i2
 	cmp ax,bx
 	JGe C2 		;если i1 >= i2 то перейти на C2
 	
 	mov res,ax
 	jmp B2
-	
 C2:
 	mov res,bx	;если i1 >= i2
-	
 B2:
 	int 20h
 		  
