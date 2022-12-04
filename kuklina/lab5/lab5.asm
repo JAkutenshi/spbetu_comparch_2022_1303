dcee86363374ac3b91d127be519705599e30e1e6
Yulia2222 <madam.kuklina2018@yandex.ru>
2022-12-04 15:54:38 +0300

    task

--- kuklina/lab5/lab5.asm
+++ kuklina/lab5/lab5.asm
@@ -1,104 +1,166 @@
 assume cs:CODE, ds:DATA, ss:AStack
 
-AStack  segment stack
-    db 1024 dup(0)
-AStack  ends
+AStack segment stack
+dw     1024 dup(0)
+AStack ends
 
-DATA    segment
-    delay   dw 2000
-    KEEP_CS dw 0    
-    KEEP_IP dw 0	  
-DATA    ends
+DATA segment
+  delay dw 2000
+  KEEP_CS dw 0    ;хранить сs
+  KEEP_IP dw 0   ;хранить ip
+  error db 'Invalid number, enter new number (100--9000)', 0ah, 0dh, '$'
+DATA ends
 
-CODE    segment
+CODE segment
 
+; dx -- input
 FUNC PROC FAR
-    push ax                           
-    push dx
-    mov  dx, ax
-    in   al, 61h                      
-    push ax                           
-    or   al, 00000011b                
-    out  61h, al                     
-    mov  al,  dl                      
-    out  42h, al                      
-    mov  cx, delay                    
+	push ax; сохранение изменяемых регистров
 
-; Задержка
-SOUND_STOP:                      
-    push cx
-    mov  cx, delay
-    SOUND_STOP_2:
-        nop                           
-        loop SOUND_STOP_2         
-    pop  cx
-    loop SOUND_STOP
+	mov  al, 10110111b
+	out  43h, al
+	in   al, 61h; получить Информация о динамике(61h)
+	push ax; и сохранить ее
+	or   al, 00000011b; установить 2 младших бита
+	out  61h, al; Включить динамик
+	mov  al, dl; высота звука
+	out  42h, al; (42h - порт управл
+	mov  al, dh
+	out  42h, al
+	mov  cx, delay; установить длительность звука
 
-    pop  ax
-    and  al, 11111100b
-    out  61h, al                      
-    
-    pop  dx                           
-    pop  ax
-    mov  al, 20h
-   
-    out  20h, al
+	;    Задержка
+SOUND_STOP:                       ; 4 000 000 раз
+	push cx
+	mov  cx, delay
 
-    iret                              
+SOUND_STOP_2:
+	nop  ; занимает мето и время
+	loop SOUND_STOP_2; loop - уменьшает cs в цикле
+	pop  cx
+	loop SOUND_STOP
+
+	pop ax
+	and al, 11111100b
+	out 61h, al; выкл
+	pop ax
+
+	mov al, 20h
+
+	out 20h, al
+
+	iret ; возврат из прерывания
 FUNC ENDP
 
+; al -> number
+scan_digit proc near
+  xor  ax, ax
+
+  mov  ah, 01h
+  int  21h
+
+  cmp  al, 0dh
+  je   enter_symbol
+  sub  al, '0'
+  and  ax, 00ffh
+  jmp  end_scan_digit
+
+enter_symbol:
+  mov  al, 10
+  and  ax, 00ffh
+
+end_scan_digit:
+  ret
+scan_digit endp
+
 MAIN PROC FAR
-    push ds
-    xor  ax, ax
-    push ax
-    mov  ax, DATA
-    mov  ds, ax
-    mov  ah, 35h                    
-    mov  al, 23h 
-    int  21h                         
-    mov  KEEP_CS, es              
-    mov  KEEP_IP, bx 
-    
-    push ds                         
-    mov  dx, offset FUNC 
-    mov  ax, seg FUNC    
-    mov  ds, ax                     
-    mov  ah, 25h                    
-    mov  al, 23h                    
-    int  21h                        
-    pop  ds
-    
-INPUT_CTRL_LOOP:                    
-    in   al, 60h                   
-    cmp  al, 1dh                    ; 1d - ctrl
-    jne  INPUT_CTRL_LOOP            
-INPUT_C_LOOP:
-    in   al, 60h
-    cmp  al, 9dh                    ; отжатие ctrl
-    je   INPUT_CTRL_LOOP            
-    cmp  al, 2eh                    ; 2e - c
-    jne  INPUT_C_LOOP
-    mov al, 10
-    int  23h                      
+	push ds
+	xor  ax, ax
+	push ax
 
-    cli                             
+	mov  ax, DATA
+	mov  ds, ax
 
-    push ds
-    mov  dx, KEEP_IP
-    mov  ax, KEEP_CS
-    mov  ds, ax
-    mov  ah, 25h
-    mov  al, 23h
-    int  21h                        
-    pop  ds
+	mov  ah, 35h
+	mov  al, 23h
+	int  21h
+	mov  KEEP_CS, es
+	mov  KEEP_IP, bx
 
-    sti                             
+  mov  cx, 4
+scan_number:
+  xor  dx, dx
+  xor  ax, ax
 
-    ret
+  call scan_digit
+  cmp  al, 10
+  je   check_number
+  add  dx, ax
+
+  call scan_digit
+  cmp  al, 10
+  je   check_number
+  shl  dx, cl
+  add  dx, ax
+
+  call scan_digit
+  cmp  al, 10
+  je   check_number
+  shl  dx, cl
+  add  dx, ax
+
+  call scan_digit
+  cmp  al, 10
+  je   check_number
+  shl  dx, cl
+  add  dx, ax
+
+check_number:
+  cmp  dx, 100h
+  jge  check_greater
+  cmp  dx, 9000h
+  jle  exit
+  jmp  print_error
+
+check_greater:
+  jmp  exit
+
+print_error:
+  mov  dx, offset error 
+  mov  ah, 09h
+  int  21h
+  jmp  scan_number
+
+exit:
+  push dx
+	push ds
+	mov  dx, offset FUNC
+	mov  ax, seg FUNC
+	mov  ds, ax
+	mov  ah, 25h
+	mov  al, 23h
+	int  21h
+	pop  ds
+  pop  dx
+
+  int  23h
+
+	cli
+
+	push ds
+	mov  dx, KEEP_IP
+	mov  ax, KEEP_CS
+	mov  ds, ax
+	mov  ah, 25h
+	mov  al, 23h
+	int  21h
+	pop  ds
+
+	sti 
+
+	ret
 
 MAIN ENDP
 
 CODE ENDS
-END MAIN
-
-
+END  MAIN
