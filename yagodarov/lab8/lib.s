3ef4ea64e7e98a60ee9ca81f961056da2bfdbf66
Michael Yagodarov <purposelessness@yandex.ru>
2022-12-05 02:02:22 +0300

    fpu using and debugging info added

--- yagodarov/lab8/lib.s
+++ yagodarov/lab8/lib.s
@@ -4,15 +4,22 @@
 #; x: double  -> xmm0
 #; n: int     -> rdi
 #; c: double* -> rsi
+#; Output:
+#; rax
 poly:
-  movsd xmm1, xmm0
-  subsd xmm0, xmm0
+  movq  rax, xmm0
+  push  rax
+  fld qword ptr [rsp]
+  fldz
   test  rdi, rdi
   jz    poly_end
   mov   rcx, rdi
 horner:
-   mulsd xmm0, xmm1
-   addsd xmm0, [rsi + rcx * 8 - 8]
-   loop  horner
+    fmul  st(1)
+    fadd qword ptr [rsi + rcx * 8 - 8]
+    loop  horner
 poly_end:
+  fstp qword ptr [rsp]
+  pop   rax
+  movq  xmm0, rax
   ret
