8f0e421285426d87a55794f3cb7295cb1acfba89
NikKropotov <nikitakropotov7@gmail.com>
2022-12-06 18:57:54 +0300

    changed

--- kropotov/lab5/lab5.asm
+++ kropotov/lab5/lab5.asm
@@ -1,124 +1,121 @@
-DATA SEGMENT 
-    SAVE_IP DW 0
-    SAVE_CS DW 0
+DATA SEGMENT
+    KEEP_CS dw 0
+    KEEP_IP dw 0
+	MyString db 10, 13, 'TLOU$'
+	OutNum dw 3
+	EndMessage db 10, 13, 'End int$'
 DATA ENDS
 
+
 AStack SEGMENT STACK
-    DW 1024 DUP(?)
+    db 1024 DUP (?)
 AStack ENDS
 
-CODE SEGMENT 
-    ASSUME CS:CODE, DS:DATA, SS:AStack
 
-SUBR_INT PROC FAR
-    push AX
-    push BX
-    push DX
-    push CX
+CODE SEGMENT
 
-    mov bx, 5	;sound frequency 
-    mov al, 0b6h
-    out 43h, al
-    mov dx, 0014h
-    mov ax, 4f38h
-    div di
+ASSUME CS:CODE, DS:DATA, SS:AStack
 
-    ; set freq
-    out 42h, al 
-    mov al, ah
-    out 42h, al
-    
-    ;sound on
-    in al, 61H 	;cur port state to al
-    mov ah, al 	;save state in ah
-    or al, 3 	;set 0 and 1 bit at 1 
-    out 61H, al ;speaker on 
-
-    l1: mov cx, 2801h
-    l2: loop l2
-    dec bx
-    jnz l1
-    mov al, ah
-    and al, 11111100b
-    out 61H, al
-
-
-    pop AX
-    pop BX
-    pop DX
-    pop CX
-    mov AL, 20H
-    out 20H, AL
-    iret
+SUBR_INT PROC FAR 
+	push ax 
+	push cx
+	push bx
+	push dx
+	
+	mov        al,0Bh
+    out        70h,al
+    in         al,71h
+    and        al,11111011b
+    out        71h,al
+    mov        al,32h
+    call       PRINT_FUNC
+    mov        al,9
+    call       PRINT_FUNC
+    mov        al,'-'
+    int        29h
+    mov        al,8
+    call       PRINT_FUNC
+    mov        al,'-'
+    int        29h
+    mov        al,7
+    call       PRINT_FUNC
+    mov        al,' '
+    int        29h
+    mov        al,4
+    call       PRINT_FUNC
+    mov        al,':'
+    int        29h
+    mov        al,2
+    call       PRINT_FUNC
+    mov        al,':' 
+    int        29h
+    mov        al,0h
+    call       PRINT_FUNC
+	mov		   al, 0dh
+	int 	   29h
+	mov 	   al, 0ah
+	int 	   29h
+	
+	pop dx
+	pop bx 
+	pop cx
+	pop ax
+	mov al, 20h
+	out 20h, al
+	iret
 SUBR_INT ENDP
 
-Main PROC FAR
-    push ds
-    sub ax,ax
-    push ax
-    mov ax, DATA
-    mov ds, ax
+PRINT_FUNC proc near
+        out        70h,al
+        in         al,71h
+        push       ax
+		mov        cl, 4
+        shr        al,cl
+        add        al,'0'
+        int        29h
+        pop        ax
+        and        al,0Fh
+        add        al,30h
+        int        29h                  
+        ret
+PRINT_FUNC endp
 
-    mov di, 10000
+Main	PROC  FAR
+	push DS
+	sub AX, AX
+	push AX
+	mov AX, DATA 
+	mov DS, AX 
 
-    MOV AH, 35H 
-    MOV AL, 08H
-    INT 21H
-    MOV SAVE_IP, BX
-    MOV SAVE_CS, ES
+	MOV AH, 35H 
+	MOV AL, 60H 
+	INT 21H
+	MOV KEEP_IP, BX 
+	MOV KEEP_CS, ES 
+	PUSH DS
+	MOV DX, OFFSET SUBR_INT 
+	MOV AX, SEG SUBR_INT 
+	MOV DS, AX 
+	MOV AH, 25H 
+	MOV AL, 60H 
+	INT 21H
+	POP DS
 
-    push DS
-    mov DX, offset SUBR_INT
-    mov ax, seg SUBR_INT
-    mov ds, ax
-    mov ah, 25h
-    mov al, 08H
-    int 21h
-    pop ds
+	int 60H
 
-    
-loop_int:
-    push ax
-    mov ah, 1h
-    int 21H
-
-    cmp al, "-"
-    je minus
-
-    cmp al, "+"
-    je plus
-
-    cmp al, 1bh
-    
-    pop ax
-    jnz loop_int
-    jmp exit
-
-plus:
-    add di, 1000
-    pop ax
-    jmp loop_int
-
-minus:
-    sub di, 1000
-    pop ax
-    jmp loop_int
-
-
-exit:
-    CLI
-    PUSH DS
-    MOV  DX, SAVE_IP
-    MOV  AX, SAVE_CS
-    MOV  DS, AX
-    MOV  AH, 25H
-    MOV  AL, 08H
-    INT  21H
-    POP  DS
-    STI
-
-    ret
-Main ENDP
-
+	CLI
+	PUSH DS
+	MOV DX, KEEP_IP 
+	MOV AX, KEEP_CS 
+	MOV DS, AX 
+	MOV AH, 25H
+	MOV AL, 60H 
+	INT 21H
+	POP DS
+	STI
+	
+	mov ah, 4ch
+	int 21h
+Main      ENDP
 CODE ENDS
-     END Main
+	END Main
