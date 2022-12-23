3ee6272271fa3815de6a1347cb5dd942b523956a
Dmitry Musatov <90715009+Lifeestyleee@users.noreply.github.com>
2022-12-23 23:26:21 +0300

    Create lab6.asm

---
+++ musatov/lab6/lab6.asm
@@ -0,0 +1,52 @@
+.MODEL FLAT, C
+.CODE
+
+func PROC C numRanDat: dword, nInt: dword, nums: dword, ints: dword, res: dword
+	push esi
+	push edi
+	push eax
+	push ebx
+	push ecx
+	push edx
+	mov ecx, numRanDat
+	mov esi, nums
+	mov edi, ints
+	mov eax, 0
+	mov edx, nInt
+	inc edx
+	lp:
+		mov ebx, 0
+		checking:
+			cmp ebx, edx
+			jge skip
+			push eax
+			mov eax, [esi + eax * 4]
+			cmp eax, [edi + ebx * 4]
+			pop eax
+			jl out_num
+			inc ebx
+			jmp checking
+		out_num:
+			dec ebx
+			cmp ebx, -1
+			je skip
+			mov esi, res
+			push eax
+			mov eax, [esi + ebx * 4]
+			inc eax
+			mov [esi + ebx * 4], eax
+			pop eax
+			mov esi, nums
+		skip:
+			inc eax
+	loop lp
+	finish:
+	pop edx
+	pop ecx
+	pop ebx
+	pop eax
+	pop edi
+	pop esi
+	ret
+func ENDP
+END
