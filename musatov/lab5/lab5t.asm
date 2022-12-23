5d3e258f997c01f0e0dd83395ec8f49a33088ee3
Dmitry Musatov <90715009+Lifeestyleee@users.noreply.github.com>
2022-12-23 23:30:25 +0300

    Add files via upload

--- musatov/lab5/lab5t.asm
+++ musatov/lab5/lab5t.asm
@@ -1,192 +1,119 @@
-assume ss:my_stack, cs:my_code, ds:my_data
+assume cs:my_code, ds:my_data, ss:my_stack
 
-my_stack segment stack
+my_stack  segment stack
   db 1024 dup(0)
-my_stack ends
+my_stack  ends
 
-my_data segment
-  int_segment dw 0
-  int_offset  dw 0
+my_data    segment
+  delay     dw 2000
+  cached_cs dw 0
+  cached_ip dw 0
+  flag      db 0
+my_data    ends
 
-  msg     db 'Some message', 0ah, 0dh, '$'
-  int_msg db 'Interruption is done', 0ah, 0dh, '$'
-  flag    db 0
-  delim   db ':'
-my_data ends
+my_code    segment
 
-my_code segment
-
-display_time proc near
-  push ax
-  push cx
-
-  mov  cl, 4
-
-; Print hours
-  mov  al, 04h
-  out  70h, al
-  in   al, 71h
-  push ax
-  shr  al, cl
-  add  al, '0'
-  int  29h
-  pop  ax
-  and  al, 00001111b
-  add  al, '0'
-  int  29h
-
-; Print delim
-  mov  al, delim
-  int  29h
-
-; Print minutes
-  mov  al, 02h
-  out  70h, al
-  in   al, 71h
-  push ax
-  shr  al, cl
-  add  al, '0'
-  int  29h
-  pop  ax
-  and  al, 00001111b
-  add  al, '0'
-  int  29h
-
-; Print delim
-  mov  al, delim
-  int  29h
-
-; Print seconds
-  mov  al, 00h
-  out  70h, al
-  in   al, 71h
-  push ax
-  shr  al, cl
-  add  al, '0'
-  int  29h
-  pop  ax
-  and  al, 00001111b
-  add  al, '0'
-  int  29h
-
-; Print new line
-  mov  al, 0ah
-  int  29h
-  mov  al, 0dh
-  int  29h
-
-  pop  cx
-  pop  ax
-  ret
-display_time endp
-
-display_message proc near ; print message from `dx` register
-  push ax
-
-  mov  ah, 09h
-  int  21h
-
-  pop  ax
-  ret
-display_message endp
-
-my_int_proc proc far
-; To run procedure only once
+; dx -- speaker sound frequency (decimal)
+my_interruption proc far
   cmp  flag, 0
-  jne  my_int_proc_end
-  mov  flag, 1
-
-  push cx
-  push dx
+  je   my_interruption_exit
+  mov  flag, 0
   push ax
 
-; Print messages
-  xor  cx, cx
-  mov  cx, 5
+  in   al, 61h                      ; Информация о динамике
+  push ax
+  or   al, 00000011b
+  out  61h, al                      ; Включить динамик, управление таймером
+  mov  al, 10110110b                ; Настройка управляющего регистра таймера
+  mov  ax, dx
+  out  42h, al                      ; Установка заданной частоты
+  mov  cl, 4
+  shr  ax, cl
+  out  42h, al
+  mov  cx, delay
 
-print_msg_loop:
-  call display_time
-  loop print_msg_loop
+; Задержка
+sound_duration:
+  push cx
+  mov  cx, delay
+  sound_duration_2:
+    nop
+    loop sound_duration_2
 
-; Wait...
-  xor  cx, cx
-  mov  cx, 20
-update_dx:
-  mov  dx, 0ffffh
-wait_loop:
-  nop
-  dec  dx
-  cmp  dx, 0
-  jne  wait_loop
-  loop update_dx
-
-; Print `end` message
-  call display_time
+  pop  cx
+  loop sound_duration
 
   pop  ax
-  pop  dx
-  pop  cx
+  and  al, 11111100b
+  out  61h, al                      ; Выключить динамик
 
-my_int_proc_end:
+  pop  ax
+
+my_interruption_exit:
   mov  al, 20h
   out  20h, al
 
   iret
-my_int_proc endp
+my_interruption endp
 
 main proc far
   push ds
-  xor  ax, ax
-  push ax
+	xor  ax, ax
+	push ax
 
   mov  ax, my_data
   mov  ds, ax
 
-; Save previous interruption info
-  mov  ah, 35h
-  mov  al, 08h
+  mov  ah, 35h                    ; сохраняем адрес старого прерывания
+  mov  al, 1ch
   int  21h
-  mov  int_offset, bx
-  mov  int_segment, es
+  mov  cached_cs, es
+  mov  cached_ip, bx
 
-; Change interruption to custom
   push ds
-
-  mov  dx, offset my_int_proc
-  mov  ax, seg my_int_proc
+  mov  dx, offset my_interruption ; смещение для процедуры в DX
+  mov  ax, seg my_interruption    ; сегмент процедуры
   mov  ds, ax
-
-  mov  ah, 25h
-  mov  al, 08h
-  int  21h
-
+  mov  ah, 25h                    ; функция установки вектора
+  mov  al, 1ch                    ; номер вектора
+  int  21h                        ; меняем прерывание
   pop  ds
 
-; Wait 1 second
+input_loop:
+  in   al, 60h
+  cmp  al, 01h
+  je   exit
+  cmp  al, 1dh
+  jne  input_loop
+input_c_loop:
+  in   al, 60h
+  cmp  al, 9dh
+  je   input_loop
+  cmp  al, 2eh
+  jne  input_c_loop
+
+  mov  flag, 1
+
   xor  ax, ax
   mov  ah, 86h
   mov  cx, 0fh
   mov  dx, 4240h
   int  15h
 
-; Restore interruption
-  cli
+exit:
+  cli                             ; Сброс флага прерывания
   push ds
-
-  mov  dx, int_offset
-  mov  ax, int_segment
+  mov  dx, cached_ip
+  mov  ax, cached_cs
   mov  ds, ax
-
   mov  ah, 25h
-  mov  al, 08h
-  int  21h
-
+  mov  al, 1ch
+  int  21h                        ; восстанавливаем старый вектор прерывания
   pop  ds
-  sti
-
+  sti                             ; Установка флага прерывания
   ret
+
 main endp
 
 my_code ends
-
 end main
