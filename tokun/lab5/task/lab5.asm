c027c4b267d494074cc63511ad5f23e3b18637ec
TokGrin <90797638+TokGrin@users.noreply.github.com>
2023-02-02 19:50:39 +0300

    Tokun_Grigory_lb5: task

--- tokun/lab5/task/lab5.asm
+++ tokun/lab5/task/lab5.asm
@@ -1,52 +1,34 @@
-ASSUME CS:CODE, DS:DATA, SS:STACK
+AStack  SEGMENT STACK
+    DB 1024 DUP(?)
+AStack  ENDS
 
-STACK SEGMENT STACK
-	DW    512 DUP(0)
-STACK ENDS
+DATA    SEGMENT
+  KEEP_CS DW 0    ; для хранения сегмента вектора прерывания
+  KEEP_IP DW 0    ; для смещения вектора прерывания
+  MESSAGE DB 'Hello world', 0dh, 0ah, '$'
+DATA    ENDS
 
-DATA SEGMENT
-  delay dw 2000
-	KEEP_CS DW 0
-	KEEP_IP DW 0
-DATA ENDS
+CODE    SEGMENT
+ASSUME CS:CODE, DS:DATA, SS:AStack
 
-CODE   SEGMENT
+WriteMsg  PROC  NEAR
+  push ax
+  mov AH, 9
+  int 21h 
+  pop ax
+  ret
+WriteMsg  ENDP
 
 FUNC PROC FAR
-  push ax
-  push cx
+	push dx
 
-  mov  al, 10110110b
-  out  43h, al
-  in   al, 61h
-  push ax
-  or   al, 00000011b
-  out  61h, al
-  mov  al, dl
-  out  42h, al
-  mov  al, dh
-  out  42h, al
-
-  mov  cx, delay
-sound_duration:
-  push cx
-  mov  cx, delay
-  sound_duration_2:
-    nop
-    loop sound_duration_2
-  pop  cx
-  loop sound_duration
-
-  pop  ax
-  and  al, 11111100b
-  out  61h, al
-  
-  pop  cx
-  pop  ax
-
-	mov  al, 20h
-  out  20h, al
-
+	mov dx, OFFSET MESSAGE
+	call WriteMsg
+	
+	pop dx
+	
+  mov al, 20h
+  out 20h, al
 	iret
 FUNC ENDP
 
@@ -54,39 +36,44 @@ MAIN PROC FAR
   push ds
   mov ax, DATA
   mov ds, ax
-  
+    
   mov ah, 35h ; функция получения вектора
-  mov al, 23h ; номер вектора
+  mov al, 08h ; номер вектора
   int 21h
   mov KEEP_IP, bx ; запоминание смещения
   mov KEEP_CS, es ; и сегмента вектора прерывания
-  
+    
   push ds
   mov dx, OFFSET FUNC ; смещение для процедуры в DX
   mov ax, SEG FUNC ; сегмент процедуры
   mov ds, ax ; помещаем в DS
   mov ah, 25h ; функция установки вектора
-  mov al, 23h ; номер вектора
+  mov al, 08h ; номер вектора
   int 21h ; меняем прерывание
   pop ds
-  
-  mov ah, 01h
-  int 21h
-  
+    
+  mov cx, 1000
+loop_1:
+  push cx
+  mov cx, 1000
+  loop_2:
+    nop
+    loop loop_2
+  pop cx
+  loop loop_1
+    
   cli
   push ds
   mov dx, KEEP_IP
   mov ax, KEEP_CS
   mov ds, ax
   mov ah, 25h
-  mov al, 23h
+  mov al, 08h
   int 21h ; восстанавливаем старый вектор прерывания
   pop ds
   sti
   mov ah, 4ch
   int 21h
 MAIN ENDP
-
 CODE ENDS
-
-END MAIN
+ END MAIN
